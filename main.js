5321ec295625330b5d508b730359bbbcb004cb86
Dvd <dvdnet22@gmail.com>
2022-08-03 11:32:46 -0500

    Fixed centering and added active links

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const theme = document.querySelector(".theme");
 const root = document.documentElement;
 const typer = document.querySelector(".typer");
+const navlinks = document.querySelectorAll(".nav-link");
 
-function themeChange() {
+navlinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		navlinks.forEach((link) => {
+			link.classList.remove("active");
+		});
+		link.classList.add("active");
+	});
+});
+
+theme.addEventListener("click", () => {
 	if (theme.classList.contains("dark")) {
 		root.style.setProperty("--background-color", "#fff");
 		root.style.setProperty("--text-color", "#000");
@@ -14,6 +24,4 @@ function themeChange() {
 		theme.innerHTML = '<ion-icon name="sunny-outline"></ion-icon>';
 		theme.classList.add("dark");
 	}
-}
-
-theme.addEventListener("click", themeChange);
+});
