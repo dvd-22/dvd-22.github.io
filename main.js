c38c8d9591e620f8811b911bea70cc188ac7759d
Dvd <dvdnet22@gmail.com>
2022-08-19 17:26:31 -0500

    Added a background on the index

--- main.js
+++ main.js
@@ -1,32 +1,36 @@
 const themeButton = document.querySelector(".theme");
 const root = document.documentElement;
 
-themeButton.addEventListener("click", () => {
-	let theme = localStorage.getItem("theme");
-	if (theme === "light") {
-		root.style.setProperty("--background-color", "#1a1a1a");
-		root.style.setProperty("--text-color", "#fff");
-		themeButton.innerHTML = '<ion-icon name="sunny-outline"></ion-icon>';
-		localStorage.setItem("theme", "dark");
-	} else {
-		root.style.setProperty("--background-color", "#fff");
-		root.style.setProperty("--text-color", "#000");
-		themeButton.innerHTML = '<ion-icon name="moon-outline"></ion-icon>';
-		localStorage.setItem("theme", "light");
-	}
-});
-
 function loadTheme() {
 	let theme = localStorage.getItem("theme");
 	if (theme === "light") {
 		root.style.setProperty("--background-color", "#fff");
 		root.style.setProperty("--text-color", "#000");
+		root.style.setProperty(
+			"--bg-shapes-color",
+			`invert(0%) sepia(100%) saturate(0%) hue-rotate(322deg)
+		brightness(100%) contrast(100%)`
+		);
 		themeButton.innerHTML = '<ion-icon name="moon-outline"></ion-icon>';
 	} else {
 		root.style.setProperty("--background-color", "#1a1a1a");
 		root.style.setProperty("--text-color", "#fff");
+		root.style.setProperty(
+			"--bg-shapes-color",
+			`invert(100%) sepia(55%) saturate(0%) hue-rotate(143deg)
+			brightness(109%) contrast(101%)`
+		);
 		themeButton.innerHTML = '<ion-icon name="sunny-outline"></ion-icon>';
 	}
 }
+
+themeButton.addEventListener("click", () => {
+	let theme = localStorage.getItem("theme");
+	theme === "light"
+		? localStorage.setItem("theme", "dark")
+		: localStorage.setItem("theme", "light");
+
+	loadTheme();
+});
 
 loadTheme();
