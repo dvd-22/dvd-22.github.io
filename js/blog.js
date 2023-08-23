2f37641c3f806f7d7b97ec950d9550626b39aa8f
Dvd <dvdnet22@gmail.com>
2023-08-23 11:27:45 -0600

    Added SEXi to projects and date to blogs

--- js/blog.js
+++ js/blog.js
@@ -2,22 +2,26 @@ const parent = document.getElementsByClassName("blogposts");
 
 let list = [
 	{
+		title: "Didn't make it to collegue",
+		date: "Aug 23rd, 2023",
+		type: "Personal",
+		path: "./didnt-make-it-to-collegue.html",
+	},
+	{
+		title: "Horizonte de Eventos",
+		date: "Nov 11, 2022",
+		type: "Tale",
+		path: "./horizonte-de-eventos.html",
+	},
+	{
 		title: "Tools List",
+		date: "Jan 02nd, 2023",
 		type: "Resource",
 		path: "./tools.html",
 	},
 	{
-		title: "Horizonte de Eventos",
-		type: "Tale",
-		path: "./horizonte-de-eventos.html",
-	},
-	// {
-	// 	title: "Hombre solitario",
-	// 	type: "Tale",
-	// 	path: "./hombre-solitario.html",
-	// },
-	{
 		title: "Carta a la pared",
+		date: "Nov 12, 2020",
 		type: "Tale",
 		path: "./carta-a-la-pared.html",
 	},
@@ -35,15 +39,28 @@ const createBlogLinks = () => {
 		a.className = "clicky-links";
 		a.innerText = list[i].title;
 
-		let div = document.createElement("div");
-		div.className = "post-type";
-		div.innerText = list[i].type;
+		let type = document.createElement("div");
+		type.className = "post-type";
+		type.innerText = `${list[i].date}\n${list[i].type}`;
+
 		li.appendChild(a);
-		li.appendChild(div);
+		li.appendChild(type);
 		parent[0].appendChild(li);
 	}
 };
 
 if (parent.length > 0) {
 	createBlogLinks();
+}
+
+const DATE = document.querySelector(".date");
+
+if (DATE) {
+	console.log();
+	for (let i = 0; i < list.length; i++) {
+		if (list[i].path === window.location.pathname.replace(/\/blog/, ".")) {
+			DATE.innerText = list[i].date;
+			break;
+		}
+	}
 }
